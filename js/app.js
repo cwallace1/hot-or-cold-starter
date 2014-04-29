7b2500c72cc665922e4f17d720d97602cedb7eef
Chase Wallace <cwallace@archermalmo.com>
2014-04-29 17:32:02 -0500

    the assignment so far

--- js/app.js
+++ js/app.js
@@ -1,17 +1,206 @@
+//set the variables that are accessed throughout
+//and need a global scope
+var goal,
+    lastGuess,
+    difference,
+    changeDiff,
+    color = "",
+    guess = "",
+    n = 0,
+    flag = true,
+    firstCheck = true;
 
+//starts the deal when doc is ready
 $(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+    timeToGuess();
+    focal();
 
-  	});
+//this is for the new game button
+    $(".new").click(function(){
+        resetAll();
+        timeToGuess();
+    });
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+//these are the two methods for inputting a guess
+    $("#guessButton").click(guessIt);
+    $(document).keypress(keyedYa(e));
 
+ //leaving these as they were
+/*--- Display information modal box ---*/
+    $(".what").click(function(){
+        $(".overlay").fadeIn(1000);
+    });
+
+    /*--- Hide information modal box ---*/
+    $("a.close").click(function(){
+        $(".overlay").fadeOut(1000);
+    });
 });
 
+//this is the function that creates a new goal
+//on page load/new game press
+function timeToGuess() {
+    if (!flag){
+        return;
+    }
+    else {
+        goal = undefined;
+        flag = false;
+        goalNumba();
+    }
+}
 
+//this is the block that runs when a user enters a guess
+function guessIt() {
+    var guessCheck;
+    guess = parseInt($("#userGuess").val());
+    guessCheck = $.isNumeric(guess);
+    changeDiff = Math.abs(goal-guess);
+//this set checks if the guess is the first of the game
+    if (firstCheck === true){
+        lastGuess = guess;
+        difference = Math.abs(goal-guess);
+        if (!guessCheck) {
+            suddenly("do it again!");
+            focal();
+        }
+        else if (guess > 100) {
+            suddenly("try to keep it under 100.");
+            focal();
+        }
+        else if (guess === goal) {
+            suddenly("you did it! are you cheating?");
+            color = "winner";
+            increment();
+        }
+        else if (10> difference) {
+            suddenly("very hot!!!");
+            color = "hot";
+            firstStack();
+        }
+        else if (10< difference && 20>difference) {
+            suddenly("warm!");
+            color = "warm";
+            firstStack();
+        }
+        else if (20< difference && 50>difference) {
+            suddenly("cold.");
+            color = "cold";
+            firstStack();
+        }
+        else if (50< difference) {
+            suddenly("probably couldn't be farther.");
+            color = "arctic";
+            firstStack();
+        }
+    }
+//if it isnt the first game, the alerts react appropriately
+    else if (!firstCheck) {
+        if (!guessCheck) {
+            suddenly("do it again!");
+            focal();
+        }
+        else if (guess > 100) {
+            suddenly("try to keep it under 100.");
+            focal();
+        }
+        else if (guess === goal){
+            suddenly("you got it!");
+            color = "winner";
+            difference = changeDiff;
+            increment();
+        }
+        else if (changeDiff===difference && guess !== lastGuess){
+            suddenly("you've shot the gap!");
+            color = "static";
+            otherStack();
+        }
+        else if (changeDiff===difference){
+            suddenly("you didnt change your guess!");
+            color = "same";
+            otherStack();
+        }
+        else if (changeDiff<difference && changeDiff<5){
+            suddenly("whoa! super hot!");
+            color = "hot";
+            otherStack();
+        }
+        else if (changeDiff>difference && changeDiff>25){
+            suddenly("brrr.... arctic...");
+            color = "arctic";
+            otherStack();
+        }
+        else if (changeDiff<difference){
+            suddenly("warmer!");
+            color = "warm";
+            otherStack();
+        }
+        else if (changeDiff>difference){
+            suddenly("colder!");
+            color = "cold";
+            otherStack();
+        }
+    }
+}
+
+//checks for enter keypress and runs the guess checker
+function keyedYa(e){
+    if (e.which == 13) {
+        $.unbind("keypress");
+        guessIt();
+    }
+}
+
+//creates the random number
+function goalNumba() {
+    goal = Math.floor((Math.random() * 100)+1);
+}
+
+//clears the previous guess and focuses on input field
+function focal() {
+    $("#userGuess").val("");
+    $("#userGuess").focus();
+}
+
+//increments the counter and adds the guess to the
+//unordered list object in the dom
+function increment() {
+    n+=1;
+    $("#count").text(n);
+    $("#guessList").append("<li class=\"added "+color+"\">"+guess+"</li>");
+}
+
+//stack of commands for the first guess block
+function firstStack() {
+    focal();
+    increment();
+    firstCheck = false;
+}
+
+//stack of commands for the other guessing block
+function otherStack() {
+    difference = changeDiff;
+    focal();
+    increment();
+    lastGuess = guess;
+}
+
+//the alert. planning on doing a div or something else.
+//alerts suck.
+function suddenly(stuff) {
+    alert(stuff);
+}
+
+//the "clear all" for the new game button
+function resetAll() {
+    flag = true;
+    firstCheck = true;
+    focal();
+    n=0;
+    changeDiff = undefined;
+    lastGuess = undefined;
+    guess = "";
+    color = "";
+    $("#count").text(n);
+    $(".added").remove();
+}
